3cad1e62ec6d6cd78e2a1a0a0413c783685abc17
Nabhan Abdulla P V <nabhan@criodo.com>
2022-04-21 04:10:10 +0000

    Update app/src/main/java/QKART_SANITY_LOGIN/Home.java

---
+++ app/src/main/java/QKART_SANITY_LOGIN/Home.java
@@ -0,0 +1,175 @@
+package QKART_SANITY_LOGIN;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Home {
+    RemoteWebDriver driver;
+    String url = "https://crio-qkart-frontend-qa.vercel.app";
+
+    public Home(RemoteWebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigateToHome() {
+        if (!this.driver.getCurrentUrl().equals(this.url)) {
+            this.driver.get(this.url);
+        }
+    }
+
+    public Boolean PerformLogout() throws InterruptedException {
+        try {
+            // Find and click on the Logout Button
+            WebElement logout_button = driver.findElement(By.className("MuiButton-text"));
+            logout_button.click();
+
+            // Wait for Logout to Complete
+            Thread.sleep(3000);
+
+            return true;
+        } catch (Exception e) {
+            // Error while logout
+            return false;
+        }
+    }
+
+        /*
+     * Returns Boolean if searching for the given product name occurs without any
+     * errors
+     */
+    public Boolean searchForProduct(String product) {
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 03: MILESTONE 1
+            // Clear the contents of the search box and Enter the product name in the search
+            // box
+
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error while searching for a product: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Returns Array of Web Elements that are search results and return the same
+     */
+    public List<WebElement> getSearchResults() {
+        List<WebElement> searchResults = new ArrayList<WebElement>() {
+        };
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 03: MILESTONE 1
+            // Find all webelements corresponding to the card content section of each of
+            // search results
+
+            return searchResults;
+        } catch (Exception e) {
+            System.out.println("There were no search results: " + e.getMessage());
+            return searchResults;
+
+        }
+    }
+
+    /*
+     * Returns Boolean based on if the "No products found" text is displayed
+     */
+    public Boolean isNoResultFound() {
+        Boolean status = false;
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 03: MILESTONE 1
+            // Check the presence of "No products found" text in the web page. Assign status
+            // = true if the element is *displayed* else set status = false
+
+            return status;
+        } catch (Exception e) {
+            return status;
+        }
+    }
+
+    /*
+     * Return Boolean if add product to cart is successful
+     */
+    public Boolean addProductToCart(String productName) {
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 05: MILESTONE 4
+            /*
+             * Iterate through each product on the page to find the WebElement corresponding
+             * to the matching productName
+             * 
+             * Click on the "ADD TO CART" button for that element
+             * 
+             * Return true if these operations succeeds
+             */
+
+            System.out.println("Unable to find the given product");
+            return false;
+        } catch (Exception e) {
+            System.out.println("Exception while performing add to cart: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Return Boolean denoting the status of clicking on the checkout button
+     */
+    public Boolean clickCheckout() {
+        Boolean status = false;
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 05: MILESTONE 4
+            // Find and click on the the Checkout button
+
+            return status;
+        } catch (Exception e) {
+            System.out.println("Exception while clicking on Checkout: " + e.getMessage());
+            return status;
+        }
+    }
+
+ 
+    /*
+     * Return Boolean denoting the status of change quantity of product in cart
+     * operation
+     */
+    public Boolean changeProductQuantityinCart(String productName, int quantity) {
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 06: MILESTONE 5
+
+            // Find the item on the cart with the matching productName
+
+            // Increment or decrement the quantity of the matching product until the current
+            // quantity is reached (Note: Keep a look out when then input quantity is 0,
+            // here we need to remove the item completely from the cart)
+
+            return false;
+        } catch (Exception e) {
+            if (quantity == 0)
+                return true;
+            System.out.println(("exception occurred when updating cart"));
+            return false;
+        }
+    }
+
+   /*
+     * Return Boolean denoting if the cart contains items as expected
+     */
+    public Boolean verifyCartContents(List<String> expectedCartContents) {
+        try {
+            // TODO: CRIO_TASK_MODULE_TEST_AUTOMATION - TEST CASE 07: MILESTONE 6
+
+            // Get all the cart items as an array of webelements
+
+            // Iterate through expectedCartContents and check if item with matching product
+            // name is present in the cart
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Exception while verifying cart contents: " + e.getMessage());
+            return false;
+        }
+    }
+}
