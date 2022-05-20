2efd2ddb39ebbf80b00ffa93c808adc29ec3a8f2
Nabhan <nabhan@criodo.com>
2022-05-20 16:47:45 +0550

    updated changes - 2022-05-20 16:27:45

--- app/src/main/java/QKART_SANITY_LOGIN/Module1/Login.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Module1/Login.java
@@ -56,6 +56,7 @@ public class Login {
         try {
             // Find the username label (present on the top right of the page)
             WebElement username_label;
+            username_label = this.driver.findElement(By.className("username-text"));
             return username_label.getText().equals(Username);
         } catch (Exception e) {
             return false;
